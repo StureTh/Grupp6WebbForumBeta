517a1e74056d04f7915d91b1305770a493d0cab3
StureTh <sturethuren@gmail.com>
2017-01-18 11:56:54 +0100

    Test does git work`?

---
+++ WebbForumMain/WebbForumData/Models/SubForumData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebbForumData.Models
+{
+    public class SubForumData
+    {
+        public Guid SubForumId { get; set; }
+
+        public string SubForumTitle { get; set; }
+
+
+    }
+}
